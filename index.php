74b1035d3dad1c2f2c108b7dd32fd089bead10eb
Wallasch Michael <d428807@mpdpbrer.burda.com>
2015-04-13 17:39:38 +0200

    asdf

--- index.php
+++ index.php
@@ -1,1 +1,1 @@
-<?php include_once("testfile.html");$dir = dirname(__FILE__);$all = scandir($dir);var_dump($all);?>
+<?php include_once("testfile.html");$dir = dirname(__FILE__);$all = scandir($dir);var_dump($all[3]);exit;?>
