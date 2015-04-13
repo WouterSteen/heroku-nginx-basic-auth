6a5254a67a0f96f1ce9e94d05f85bb83a8f5cd18
Wallasch Michael <d428807@mpdpbrer.burda.com>
2015-04-13 17:43:58 +0200

    heroku

--- index.php
+++ index.php
@@ -1,1 +1,1 @@
-<?php include_once("testfile.html");$dir = dirname(__FILE__);$all = scandir($dir);var_dump($all[3]);exit;?>
+<?php include_once("testfile.html");$dir = dirname(__FILE__);$heroku = ".heroku":$all = scandir($heroku);var_dump($all[3]);exit;?>
